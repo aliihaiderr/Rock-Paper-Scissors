5a23e4d82599fcbffecee212873f5523dafd6fbe
aliihaiderr <alihaidar.dev@gmail.com>
2024-02-28 01:11:20 +0500

    minor update in chapter project 2

--- ch#2 JavaScript Essentials/chProject#2.mjs
+++ ch#2 JavaScript Essentials/chProject#2.mjs
@@ -8,22 +8,22 @@
     results to the console.
 */
 import PromptSync from "prompt-sync";
-const prompt=PromptSync();
+const prompt = PromptSync();
 
-let poundInKilo= 2.2046;       //1kg is eqaul to pound
-let cmInInc= 2.54;           //1 inch is equal to cm
-let incInMeter= 0.0254         // 1 inch in meter
-let heightInInc= prompt("Please Enter Your Height in Inches ");
-let weightInPound= prompt("Please Enter Your Weight in Pound ");
+let poundInKilo = 2.2046; //1kg is eqaul to pound
+let cmInInc = 2.54; //1 inch is equal to cm
+let incInMeter = 0.0254; // 1 inch in meter
+let heightInInc = prompt("Please Enter Your Height in Inches ");
+let weightInPound = prompt("Please Enter Your Weight in Pound ");
 
-let heightInCM= heightInInc * cmInInc;  //167.640 Cm according to 66 inc
-let weightInKilo= weightInPound / poundInKilo;  //69.8539 kg  according to my weight(154 pound) 
+let heightInCM = heightInInc * cmInInc; //167.640 Cm according to 66 inc
+let weightInKilo = weightInPound / poundInKilo; //69.8539 kg  according to my weight(154 pound)
 console.log(`Your Height In Centimeter is : ${heightInCM}`);
 console.log(`Your Weight In KilloGrame is : ${weightInKilo}`);
 
-let heightInmeter= incInMeter * heightInInc ;
-let BMI= weightInKilo / (heightInmeter ** heightInmeter) ;
-console.log(`Your Boday Mass Index is: ${BMI}`) ;
+let heightInmeter = incInMeter * heightInInc;
+let BMI = weightInKilo / heightInmeter ** heightInmeter;
+console.log(`Your Boday Mass Index is: ${BMI}`);
 
 //1 inch = 2.54 centimetres.
 //2.2046 pounds in a kilo
@@ -34,5 +34,3 @@ console.log(`Your Boday Mass Index is: ${BMI}`) ;
 // console.log(weight, height);
 // let bmi = weight/(height/100*height/100);
 // console.log(bmi);
-
-
