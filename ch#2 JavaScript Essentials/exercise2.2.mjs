26b6748cbccbae8445fbd3ed061c12dd64016300
aliihaiderr <alihaidar.dev@gmail.com>
2024-02-27 22:46:37 +0500

    minor update

--- ch#2 JavaScript Essentials/exercise2.2.mjs
+++ ch#2 JavaScript Essentials/exercise2.2.mjs
@@ -5,10 +5,9 @@
    simply practice the variables with string and call variable
    betwwen string with backticks(``) & without concatination operator (+) */
 
-let name="ali haider";
-let age= 26;
-let isProgramJavascript= true;
-
+let name = "ali haider";
+let age = 27;
+let isProgramJavascript = true;
 
 let msg = `Hello, my name is ${name}, I am ${age} years old and I can code javaScript: ${isProgramJavascript}`;
 console.log(msg);
