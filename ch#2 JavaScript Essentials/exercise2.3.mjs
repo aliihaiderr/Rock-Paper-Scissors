d0e04974bfff6f32e18be5044903d8a6c9af1070
aliihaiderr <alihaidar.dev@gmail.com>
2024-02-28 00:05:05 +0500

    minor update in 2.3 & 2.4

--- ch#2 JavaScript Essentials/exercise2.3.mjs
+++ ch#2 JavaScript Essentials/exercise2.3.mjs
@@ -1,24 +1,17 @@
-
-      /*You can use prompt() to get the value for a and b. Write code to get
+/*You can use prompt() to get the value for a and b. Write code to get
         the value from the user for a and b . Then square the values of both
         a and b before adding them together and finding the square root.
         Print your answer to the console.
          */
 
 import PromptSync from "prompt-sync";
-const prompt=PromptSync();
+const prompt = PromptSync();
 
+let a = prompt("Please enter the value of A : ");
+let b = prompt("Please enter the value of B : ");
 
-let a = prompt("Please enter the value of a ");
-let b =prompt("Please enter the value of b ");
+a = Number(a) ** 2;
+b = Number(b) ** 2;
 
-// a=Number(a) **2;
-// b=Number(b) **2;
-// let c= a+b;
-// console.log("The square root of a & b :"+ c);
-
-
-a = Number(a);
-b = Number(b);
-let hypotenuseVal = ((a * a) + (b * b))**0.5;
+let hypotenuseVal = (a + b) ** 0.5;
 console.log(hypotenuseVal);
--- ch#2 JavaScript Essentials/exercise2.4.mjs
+++ ch#2 JavaScript Essentials/exercise2.4.mjs
@@ -1,19 +1,18 @@
 /* Create variables for three numbers: a, b, and c. Update these
    variables with the following actions using the assignment operators */
 
-let a = 10;      //here we are hardcode the value of all the variables
-let b = 8;       //we can also get value of a,b & c by prompt.
+let a = 10; //here we are hardcode the value of all the variables
+let b = 8; //we can also get value of a,b & c by prompt.
 let c = 24;
 
 //Add b to a
 
-a= a+b;    //we can also write a +=b;
+a = a + b; //we can also write a +=b;
 console.log(a);
 //Divide a by c
-b= a / c;
+b = a / c;
 console.log(b);
 //Replace the value of c with the modulus of c and b
 
-c= c % b;
+c = c % b;
 console.log(c);
-
